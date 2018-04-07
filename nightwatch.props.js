5327e302898b1f462d7b50d60cc87285fb6da92d
Jake Barrett <Jake@Jakes-MacBook.local>
2018-04-07 12:26:38 -0600

    deleted files

--- nightwatch.props.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    resourcePath: "/Users/Jake/Desktop/DevMtn/testing-resources/",
-    seleniumServer: "selenium-server-standalone-3.11.0.jar",
-    chromedriver: "chromedriver"
- }
